83274818df603fa09fcf01467ff82916eca92df5
Камиль <94285493+kamil19862307@users.noreply.github.com>
2024-08-24 20:08:50 +0300

    feat(Query Builder): CRUD samples
    Useful examples for the future

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,101 +3,98 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\View;
+use function Laravel\Prompts\password;
 
 class HomeController extends Controller
 {
     public function index(): \Illuminate\View\View
     {
-//        $users = json_decode(file_get_contents('https://jsonplaceholder.typicode.com/users'), true);
-//        $users = [];
+        $users = [];
 
-        // Позиционные плэйсхолдеры
-//        $users = DB::select('select id, name, email from users where id > ? and name != ?', [1, 'kamil']);
+        $users = DB::table('users')->get(['name', 'id', 'email']);
 
-        $data = [
-            'id' => 1,
-            'name' => 'Isabel'
-        ];
+//        $cities = DB::table('city')->whereIn('id', [1, 2, 3])->get(['ID', 'Name']);
 
-//        $cnt_users = DB::select('select count(*) as cnt from users');
+//        $cities = DB::table('city')->where('Name', 'Like', '%mos%' )->get();
 
-        $cnt_users = DB::scalar('select count(*) as cnt from users');
+//        $query = DB::table('users')
+//            ->whereDate('email_verified_at', '>', '2024-08-11' )
+//            ->get();
 
-        // Именованные плэйсхолдеры
-        $users = DB::select(
-            'select id, name, email from users where id > :id and name != :name',
-            $data
-        );
+//        $query = DB::table('city')
+//            ->select('city.ID', 'city.Name', 'city.CountryCode', 'country.Name as country_name')
+//            ->leftJoin('country', 'city.CountryCode', '=', 'country.Code')
+//            ->limit(10)
+//            ->offset(10)
+//            ->get();
 
-        // Заполнение полей
-//        DB::insert('insert into users (name, email, password) values (?, ?, ?)',
-//            ['John Edgard', 'john@mail.com', 'password']);
+//        $query = DB::table('city')
+//            ->selectRaw('sum(Population) as sum_of_population, CountryCode')
+//            ->groupBy('CountryCode')
+//            ->having('sum_of_population', '>', 5_000_000)
+//            ->get();
 
-        // Именованные плэйсхолдеры
-//        DB::insert("insert into users (name, email, password) values (:name, :email, :password)", [
-//            'email' => 'johnTrav@mail.com',
-//            'name' => 'John Travolta',
-//            'password' => 'password'
-//            ]
+//        dump(DB::select('select @@sql_mode'));
+//
+//        DB::statement(
+//        "set sql_mode =
+//                'STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION'"
+//        );
+//
+//        dump(DB::select('select @@sql_mode'));
+
+//        $query = DB::table('city')
+//            ->selectRaw('sum(Population) as sum_of_population, CountryCode, Name')
+//            ->groupBy('CountryCode')
+//            ->having('sum_of_population', '>', 5_000_000)
+//            ->get();
+
+//        config()->set('database.connections.mysql.strict', false);
+//
+//        DB::reconnect();
+
+//        $query = DB::table('users')->insert([
+//            [
+//                'name' => 'John2 Dou',
+//                'email' => 'john2@gmail.com',
+//                'password' => 'password'
+//            ],
+//            [
+//                'name' => 'John3 Dou',
+//                'email' => 'john3@gmail.com',
+//                'password' => 'password'
+//            ],
+//        ]);
+
+//        $query = DB::table('users')->insertGetId([
+//                'name' => 'John4 Dou',
+//                'email' => 'john4@gmail.com',
+//                'password' => 'password'
+//        ]);
+
+//        $query = DB::table('users')
+//            ->where('id', '=', 32)
+//            ->update([
+//            'name' => 'John44 Dou',
+//            'email' => 'john44@gmail.com',
+//            'password' => 'password44'
+//        ]);
+
+//        $query = DB::table('users')->updateOrInsert(
+//            ['email' => 'john3@gmail.com'],
+//            ['name' => 'John33', 'password' => 'password33']
 //        );
 
-//        $id = 17;
-//
-//        $password = Hash::make('password');
-//
-//        DB::update('update users set password = :password where id = :id', [
-//            ':password' => $password,
-//            'id' => $id
-//            ]);
-
-//        DB::update('update users set email_verified_at = :cur_time where id = :id', [
-//            ':cur_time' => now(),
-//            'id' => 17
-//            ]);
-
-//        DB::update('update users set email_verified_at = :cur_time where id = :id', [
-//            ':cur_time' => date('Y-m-d, H:i:s'),
-//            'id' => 18
-//            ]);
-
-//        DB::delete('delete from users where id = :id', ['id' => 12]);
-
-//        try {
-//            DB::transaction(function (){
-//                DB::insert('insert into users (name, email, password) values (?, ?, ?)',
-//                    ['Edgard Poo', 'poo@mail.com', 'password']);
-//
-//                DB::insert('insert into users (name, email, password) values (?, ?, ?)',
-//                    ['Edgard Poo', 'poo2@mail.com', 'password']);
-//            });
-//
-//        }catch (\Exception $exception){
-//            dump($exception->getMessage());
-//        }
-
-//        try {
-//            DB::beginTransaction();
-//
-//                DB::insert('insert into users (name, email, password) values (?, ?, ?)',
-//                    ['Edgard Poo', 'poo@mail.com', 'password']);
-//
-//                DB::insert('insert into users (name, email, password) values (?, ?, ?)',
-//                    ['Edgard Poo', 'poo@mail.com', 'password']);
-//
-//                DB::commit();
-//
-//        }catch (\Exception $exception){
-//            DB::rollBack();
-//
-//            dump($exception->getMessage());
-//        }
+//        $query = DB::table('users')->delete(31);
+//        dump($query);
 
 
-        return view('home.index', compact('users', 'cnt_users'));
+        return view('home.index', compact('users'));
     }
 
     public function show(): \Illuminate\View\View
